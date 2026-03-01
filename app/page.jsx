6d8c6a7057aba3bb6f364e8be98df06804d4eb15
Victor Veloso <85770164+vitovls@users.noreply.github.com>
2026-03-01 00:37:50 +0000

    fix: Arrumando arquivos com npx create-next@13

---
+++ app/page.jsx
@@ -0,0 +1,4 @@
+export default function Home() {
+  return <h1>Sandra, eu amo você. Se você me ama, dá uma risadinha!</h1>
+};
+
--- pages/index.js
+++
@@ -1,5 +0,0 @@
-function Page() {
-    return <h1>Sandra, eu amo você. Se você me ama, dá uma risadinha!</h1>
-}
-
-export default Page;
